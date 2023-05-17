dcf58bdee66c5bacafecccb2087e344292d9b5fe
Leo McMahonlab server <haha978@naver.com>
2023-05-17 00:19:22 -0400

    added some print statements

--- Circuit.py
+++ Circuit.py
@@ -34,8 +34,6 @@ def ALA(circ, N_qubits, var_params, h_l, n_layers):
     m, n = 3, 3
     nn_dict = get_nn_dict(m, n, next_nn = False)
     next_nn_dict = get_nn_dict(m, n, next_nn = True)
-    print(nn_dict)
-    print(next_nn_dict)
     for _ in range(n_layers):
         for i in range(N_qubits):
             circ.ry(var_params[param_idx], i)
@@ -92,7 +90,7 @@ def Q_Circuit(m, n, var_params, h_l, n_layers, ansatz_type):
         return HVA(circ, m, n, var_params, h_l, n_layers)
     else:
         raise ValueError("No available ansatz")
-if __name__ == "__main__":
-    Nparams = 2*(9+9)
-    param = np.random.uniform( low = -np.pi, high = np.pi, size =Nparams)
-    print(Q_Circuit(3, 3, param, [], 2, "ALA"))
+# if __name__ == "__main__":
+#     Nparams = 2*(9+9)
+#     param = np.random.uniform( low = -np.pi, high = np.pi, size =Nparams)
+#     print(Q_Circuit(3, 3, param, [], 2, "ALA"))
